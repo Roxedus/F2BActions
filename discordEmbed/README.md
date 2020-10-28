ec8b243d66246d3bcc2e3a4de696827c726b6818
Roxedus <me@roxedus.dev>
2020-10-28 17:30:59 +0100

    Initial commit

---
+++ discordEmbed/README.md
@@ -0,0 +1,61 @@
+# discordEmbed
+
+Python script adding embed-based notifications for discord to Fail2Ban.
+
+Due to the power of f-strings, this script requires python3.6 or higher
+
+## How to use
+
+This script needs maxminds geolite2 database for best functionality. Get yours [here](https://dev.maxmind.com/geoip/geoip2/geolite2/)
+
+[Discord webhook](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks), it just need the last parts. ```40832456738934/7DcEpWr5V24OIEIELjg-KkHky86SrOgTqA```
+
+[Your discord ID](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-). ```120970603556503552```
+
+[Map API Key](https://developer.mapquest.com/) get a key from mapquest.
+
+Arguments you can send to the action:
+
+```py
+usage: discordEmbed.py [-h] -a {unban,ban,start,stop,test} [-d DB] [-f FAIL] [-i IP] [-j JAIL] [-m MAP_KEY] [-t TIME] [-u USER] [-w HOOK]
+
+Discord notifier for F2B
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a {unban,ban,start,stop,test}, --action {unban,ban,start,stop,test}
+                        Which F2B action triggered the script
+  -d DB, --db DB        Location to geoip database
+  -f FAIL, --fail FAIL  Amount of attempts done
+  -i IP, --ip IP        Ip which triggered the action
+  -j JAIL, --jail JAIL  jail which triggered the action
+  -m MAP_KEY, --map-key MAP_KEY
+                        API key for mapquest
+  -t TIME, --time TIME  The time the action is valid
+  -u USER, --user USER  Discord user, if it is a id, it will tag
+  -w HOOK, --hook HOOK  Discord hook to use.
+  ```
+
+Arguments **override** enviroment variables. This means you can define annother Discord webhook or Discord user per action. (You can also use annoter map api key if  you really want)
+
+| __Argument__  | __Eviroment Variable__ | __Description__ |
+| --- | --- | :--- |
+|`Action` | | Hopefully self-explainatory. **Required**|
+|`DB` | `F2B_GEO_DB` | Pass this variable with the action if your geolite database is not located as `/config/geoip2db/GeoLite2-City.mmdb`. |
+|`FAIL` | | This is only cosmetic, it lists the ammounts of fail-attempts invoking the ban. |
+|`HOOK` | `F2B_DISCORD_HOOK` | Pass this as an argument if you want to override the channel the webhook goes to. |
+|`IP` | | Used for IP lookup, and map. |
+|`JAIL` | | This is only cosmetic, it identifies the jail invoking the ban. |
+|`MAP_KEY` | `F2B_MAP_KEY` | Override the map key for this action. |
+|`USER` | `F2B_DISCORD_USER` | Override the mentioned user for this action. Use `None` to disable |
+|`TIME` | | If given a number, using `<bantime>` it will use this number to calculate the unban time. If given a string, like `24 hours`, it will not do this calculation. |
+
+Example Ban action, where MAP_KEY, USER and HOOK is enviroment variables
+
+```ini
+actionban = python3 /config/fail2ban/Fail2Ban.py -a ban -j <name> -i <ip> -t <bantime> -f <failures> -d /config/geoip2/GeoLite2-City.mmdb
+```
+
+## Example
+
+![Example picture](.assets/Example.png)1
---
+++ discordEmbed/discordEmbed.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+"""
+Script to send webhooks from fail2ban actions
+"""
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
+from ipaddress import ip_address
+from os import getenv
+
+from geoip2 import database
+from geoip2.errors import AddressNotFoundError
+from pytz import timezone
+from requests import Session, post
+
+
+def createPayload(args):
+    """
+    Creates the payload
+    """
+
+    discordUrl = "https://discord.com/api/webhooks/"
+    hookUser = "Fail2Ban"
+    action = args.action
+    fails = args.fail
+    hook = args.hook
+    ip = args.ip
+    jail = args.jail
+    time = args.time
+    user = args.user
+
+    webhook = {
+        "embeds": [
+            {
+                "author": {"name": hookUser},
+                "timestamp": str(datetime.utcnow())
+            }
+        ]
+    }
+
+    if not user:
+        pass
+    elif user == "None":
+        pass
+    elif user.isnumeric():
+        webhook["content"] = f"<@{int(user)}>"
+    else:
+        webhook["content"] = user
+
+    embed = {}
+    if "ban" in action:
+
+        data = getGeoData(args)
+
+        if data.get('map_embed'):
+            embed["url"] = "https://db-ip.com/" + ip
+            embed["fields"] = [{"name": f":flag_{data['iso'].lower()}:", "value": data["city"]
+                                or data["name"], "inline": True}]
+        if data.get('map_embed'):
+            embed["image"] = {"url": data['map_embed']}
+
+    if action == "ban":
+        embed["title"] = f"New ban on `{jail}`" if jail else "New ban"
+        embed["color"] = 16194076
+        embed["fields"].append({"name": "Map", "value": f"[Link]({data['map_url']})", "inline": True})
+        embed["fields"].append({"name": "Unban cmd", "value": f"```bash\nfail2ban-client unban {ip}\n```"})
+
+        if not time.isnumeric():
+            description = f"**{ip}** got banned for `{int(time)}` after `{fails}` tries"
+        else:
+            tz = timezone(getenv('TZ', 'UTC'))
+            time_ = (datetime.now(tz) + timedelta(0, float(time))).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+            description = f"**{ip}** got banned for `{fails}` failed attempts, unbanning at `{time_}`"
+        embed["description"] = description
+
+    elif action == "unban":
+        embed["title"] = f"Revoked ban on `{jail}`"
+        embed["description"] = f"**{ip}** is now unbanned"
+        embed["color"] = 845872
+
+    elif action == "start":
+        webhook["content"] = ""
+        embed["description"] = f"Started `{jail}`"
+        embed["color"] = 845872
+
+    elif action == "stopped":
+        webhook["content"] = ""
+        embed["description"] = f"Stopped `{jail}`"
+        embed["color"] = 16194076
+
+    webhook["embeds"][0].update(embed)
+    post(url=discordUrl + hook, json=webhook)
+
+
+def getGeoData(args):
+    """
+    Gets the required Geo based data
+    """
+
+    data = {}
+    geoIpDB = args.db
+    mapKey = args.map_key
+    ip = args.ip
+    private = not ip_address(ip).is_private
+
+    reader = None
+    if private:
+        try:
+            reader = database.Reader(geoIpDB)
+        except FileNotFoundError:
+            pass
+        except AddressNotFoundError:
+            pass
+        except Exception as e:
+            print('FATAL ERROR: %s', e)
+
+    if reader:
+        city = reader.city(ip)
+        geoData = {'iso': city.country.iso_code, "name": city.country.name, "city": city.city.name}
+        lat = city.location.latitude
+        lon = city.location.longitude
+        reader.close()
+        data = {**data, **geoData}
+
+        session = Session()
+
+        url_params = {"center": f"{lat},{lon}", "size": "500,300"}
+        data['map_url'] = session.get('https://mapquest.com/', params=url_params).url
+
+        try:
+            img_params = {"center": f"{lat},{lon}", "size": "500,300", "key": mapKey}
+            response = session.get('https://www.mapquestapi.com/staticmap/v5/map', params=img_params)
+            assert response.status_code == 200
+            data['map_embed'] = response.url
+        except AssertionError:
+            print('Map api not found')
+        session.close()
+    return data
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Discord notifier for F2B')
+    parser.add_argument('-a', '--action', help="Which F2B action triggered the script",
+                        required=True, choices=["unban", "ban", "start", "stop", "test"])
+    parser.add_argument('-d', '--db', help="Location to geoip database")
+    parser.add_argument('-f', '--fail', help="Amount of attempts done")
+    parser.add_argument('-i', '--ip', help="Ip which triggered the action", default="1.1.1.1")
+    parser.add_argument('-j', '--jail', help="jail which triggered the action")
+    parser.add_argument('-m', '--map-key', help="API key for mapquest")
+    parser.add_argument('-t', '--time', help="The time the action is valid")
+    parser.add_argument('-u', '--user', help="Discord user, if it is a id, it will tag")
+    parser.add_argument('-w', '--hook', help="Discord hook to use.")
+
+    args = parser.parse_args()
+
+    if not args.db:
+        if getenv('F2B_GEO_DB'):
+            args.db = getenv('F2B_GEO_DB')
+        else:
+            args.db = '/config/geoip2db/GeoLite2-City.mmdb'
+    if not args.hook:
+        args.hook = getenv('DISC_HOOK') or getenv('F2B_DISCORD_HOOK')
+    if not args.map_key:
+        args.map_key = getenv('DISC_API') or getenv('F2B_MAP_KEY')
+    if not args.user:
+        args.user = getenv('DISC_ME') or getenv('F2B_DISCORD_USER') or None
+
+    createPayload(args)
